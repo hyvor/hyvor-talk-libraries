8d68416cf3b4bcdb643009fe1ecb6af5552838ec
SupunKavinda <supunkavinda1125@gmail.com>
2023-05-02 14:00:15 +0200

    minor fixes

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
-        /* dts({
-            insertTypesEntry: true
-        }), */
     ],
     build: {
         lib: {
