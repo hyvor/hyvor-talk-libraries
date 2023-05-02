d3d4cb84d81cecff088fa7dd6c0a4b42d850f96e
SupunKavinda <supunkavinda1125@gmail.com>
2023-05-02 13:51:52 +0200

    remove dts plugin

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -6,9 +6,9 @@ import dts from 'vite-plugin-dts';
 export default defineConfig({
     plugins: [
         react(),
-        dts({
+        /* dts({
             insertTypesEntry: true
-        }),
+        }), */
     ],
     build: {
         lib: {
